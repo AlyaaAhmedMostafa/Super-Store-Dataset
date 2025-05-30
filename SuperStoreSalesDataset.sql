1a2f5217cce9dedb49916a5b01eeceabb334a8fd
Alyaa Ahmed <148725925+AlyaaAhmedMostafa@users.noreply.github.com>
2025-05-30 22:21:49 +0300

    Add files via upload

---
+++ SuperStoreSalesDataset.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{DF161FE7-61A4-47A0-883B-4D4C8EBDEF8E}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {510C86E4-6C4D-4421-A1F4-8B84AF3DF7A9}
+	EndGlobalSection
+EndGlobal
